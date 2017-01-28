07ee4f10c72caa22c59591e4b44506c7c0704269
leoball <liyang.ucla@outlook.com>
2017-01-28 11:13:53 -0800

    PROJECT 3

---
+++ project3/project3/actor.cpp
@@ -0,0 +1,10 @@
+//
+//  main.cpp
+//  project3
+//
+//  Created by li yang on 2/14/16.
+//  Copyright © 2016 li yang. All rights reserved.
+//
+
+#include <iostream>
+
---
+++ project3/project3/actor.h
@@ -0,0 +1,13 @@
+//
+//  actor.h
+//  project3
+//
+//  Created by li yang on 2/14/16.
+//  Copyright © 2016 li yang. All rights reserved.
+//
+
+#ifndef actor_h
+#define actor_h
+
+
+#endif /* actor_h */
